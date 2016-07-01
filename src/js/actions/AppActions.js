f9ed1c5d7530bcf55913e27fc1629e37481d5b79
znss1989 <znss1989@gmail.com>
2016-07-01 09:55:19 +0800

    structure to flux

---
+++ src/js/actions/AppActions.js
@@ -0,0 +1,6 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+
+var AppActions = {};
+
+module.exports = AppActions;
---
+++ src/js/components/App.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+function getAppState() {
+    return {};
+};
+
+var App = React.createClass({
+    getInitialState: function() {
+        return getAppState();
+    },
+    componentDidMount: function() {
+        AppStore.addChangeListener(this._onChange);
+    },
+    componentWillUnmount: function() {
+        AppStore.removeChangeListener(this._onChange);
+    },
+    render: function() {
+        return (
+            <div>
+                My App
+            </div>
+        );
+    },
+    _onChange: function() {
+        this.setState(getAppState());
+    }
+});
+
+module.exports = App;
