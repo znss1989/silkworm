d4a75bb451812c3554ce2a4135c0a703265a85c0
znss1989 <znss1989@gmail.com>
2016-07-17 16:50:28 +0800

    clear redundant in plan item

--- dist/js/app.js
+++ dist/js/app.js
@@ -20418,27 +20418,9 @@ var AppStore = require('../stores/AppStore');
 var PlanItem = React.createClass({displayName: "PlanItem",
     getInitialState: function() {
         return {
-            isTitleEditing: false,
-            isDescriptionEditing: false,
             title: this.props.title || '',
             description: this.props.description || ''
         };
-    },
-    _onTitleDoubleClick: function() {
-        this.setState({isTitleEditing: true});
-    },
-    _onDescriptionDoubleClick: function() {
-        this.setState({isDescriptionEditing: true});
-    },
-    _onTitleChange: function(event) {
-        this.setState({
-            title: event.target.value
-        });
-    },
-    _onDescriptionChange: function(event) {
-        this.setState({
-            description: event.target.value
-        });
     },
     _onSelect: function(event) {
         var plan_id = this.props.plan_id;
@@ -20447,38 +20429,14 @@ var PlanItem = React.createClass({displayName: "PlanItem",
     _onRemove: function(event) {
         AppActions.deletePlan(this.props.plan_id);
     },
-    // savePlanChange: function(event) {
-    //     event.preventDefault(); // prevent reloading
-    //     var payload = {
-    //         planTitle: this.state.title,
-    //         planDescription: this.state.description
-    //     };
-    //     AppActions.updatePlanText(payload);
-    // },
     render: function() {
         var title = this.props.title;
         var description = this.props.description;
-        console.log(this.state.title);
-
-        // Show prompt and hide orignal content when editing
-        var titlePrompt = (this.state.isTitleEditing) ? (
-            React.createElement("div", {className: "titlePrompt"}, 
-                React.createElement("label", {htmlFor: "plan-title-prompt"}, "Plan title"), 
-                React.createElement("input", {className: "card-title", id: "plan-title-prompt", type: "text", placeholder: this.state.title, value: this.state.title, onChange: this._onTitleChange, onBlur: this._onSave})
-            )
-        ) : null;
-
-        var descriptionPrompt = (this.state.isDescriptionEditing) ? (
-            React.createElement("div", {className: "descriptionPrompt"}, 
-                React.createElement("label", {htmlFor: "plan-description-prompt"}, "Description"), 
-                React.createElement("input", {className: "card-text", id: "plan-description-prompt", type: "text", placeholder: this.state.description, value: this.state.description, onChange: this._onDescriptionChange, onBlur: this._onSave})
-            )
-        ) : null;
 
         return (
             React.createElement("div", {className: "card card-block m-y-1 p-y-1"}, 
                 React.createElement("div", {className: "container"}, 
-                    React.createElement("h4", {className: classNames('card-title', 'col-xs-10', 'p-l-0', {'editing': this.state.isTitleEditing}), onDoubleClick: this._onTitleDoubleClick}, 
+                    React.createElement("h4", {className: "card-title col-xs-10 p-l-0"}, 
                         title
                     ), 
 
@@ -20486,10 +20444,7 @@ var PlanItem = React.createClass({displayName: "PlanItem",
 
                 ), 
                 React.createElement("hr", {className: "hr-divider"}), 
-
-                titlePrompt, 
-                React.createElement("p", {className: classNames('card-text', {'editing': this.state.isDescriptionEditing}), onDoubleClick: this._onDescriptionDoubleClick}, description), 
-                descriptionPrompt, 
+                React.createElement("p", {className: "card-text"}, description), 
                 React.createElement("div", {className: "btn-group"}, 
                     React.createElement("button", {className: "btn btn-info", onClick: this._onSelect}, "Select"), 
                     React.createElement("button", {className: "btn btn-warning", onClick: this._onRemove}, "Delete")
--- src/js/components/PlanItem.react.jsx
+++ src/js/components/PlanItem.react.jsx
@@ -9,27 +9,9 @@ var AppStore = require('../stores/AppStore');
 var PlanItem = React.createClass({
     getInitialState: function() {
         return {
-            isTitleEditing: false,
-            isDescriptionEditing: false,
             title: this.props.title || '',
             description: this.props.description || ''
         };
-    },
-    _onTitleDoubleClick: function() {
-        this.setState({isTitleEditing: true});
-    },
-    _onDescriptionDoubleClick: function() {
-        this.setState({isDescriptionEditing: true});
-    },
-    _onTitleChange: function(event) {
-        this.setState({
-            title: event.target.value
-        });
-    },
-    _onDescriptionChange: function(event) {
-        this.setState({
-            description: event.target.value
-        });
     },
     _onSelect: function(event) {
         var plan_id = this.props.plan_id;
@@ -38,38 +20,14 @@ var PlanItem = React.createClass({
     _onRemove: function(event) {
         AppActions.deletePlan(this.props.plan_id);
     },
-    // savePlanChange: function(event) {
-    //     event.preventDefault(); // prevent reloading
-    //     var payload = {
-    //         planTitle: this.state.title,
-    //         planDescription: this.state.description
-    //     };
-    //     AppActions.updatePlanText(payload);
-    // },
     render: function() {
         var title = this.props.title;
         var description = this.props.description;
-        console.log(this.state.title);
-
-        // Show prompt and hide orignal content when editing
-        var titlePrompt = (this.state.isTitleEditing) ? (
-            <div className="titlePrompt">
-                <label htmlFor="plan-title-prompt">Plan title</label>
-                <input className="card-title" id="plan-title-prompt" type="text" placeholder={this.state.title} value={this.state.title} onChange={this._onTitleChange} onBlur={this._onSave} />
-            </div>
-        ) : null;
-
-        var descriptionPrompt = (this.state.isDescriptionEditing) ? (
-            <div className="descriptionPrompt">
-                <label htmlFor="plan-description-prompt">Description</label>
-                <input className="card-text" id="plan-description-prompt" type="text" placeholder={this.state.description} value={this.state.description} onChange={this._onDescriptionChange} onBlur={this._onSave} />
-            </div>
-        ) : null;
 
         return (
             <div className="card card-block m-y-1 p-y-1">
                 <div className="container">
-                    <h4 className={classNames('card-title', 'col-xs-10', 'p-l-0', {'editing': this.state.isTitleEditing})} onDoubleClick={this._onTitleDoubleClick}>
+                    <h4 className="card-title col-xs-10 p-l-0">
                         {title}
                     </h4>
 
@@ -77,10 +35,7 @@ var PlanItem = React.createClass({
 
                 </div>  
                 <hr className="hr-divider" />                
-
-                {titlePrompt}
-                <p className={classNames('card-text', {'editing': this.state.isDescriptionEditing})} onDoubleClick={this._onDescriptionDoubleClick}>{description}</p>
-                {descriptionPrompt}
+                <p className="card-text">{description}</p>
                 <div className="btn-group">
                     <button className="btn btn-info" onClick={this._onSelect}>Select</button>
                     <button className="btn btn-warning" onClick={this._onRemove}>Delete</button>
